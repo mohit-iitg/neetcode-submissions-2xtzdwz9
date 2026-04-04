3984ba32582f9243cca43149763f4d099414923f
Mohit Kumar <70018787+mohit-iitg@users.noreply.github.com>
2026-04-04 13:23:04 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stk = new ArrayDeque<Character>();
+        for(char ch:s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else if(ch == ')') {
+                if(stk.size() <= 0 || stk.peek() != '(') return false;
+                stk.pop();
+            } else if(ch == '}') {
+                if(stk.size() <= 0 || stk.peek() != '{') return false;
+                stk.pop();
+            } else if(ch == ']') {
+                if(stk.size() <= 0 || stk.peek() != '[') return false;
+                stk.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return (stk.size() > 0)?false:true;
+    }
+}
